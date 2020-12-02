577ed144b6a4e3c28c0f9ef8d089bc205aad77d3
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2020-12-01 23:30:19 -0300

    Change structure of app and add clearCompleted function

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
     Sortable.create(listSortable, {
         animation: 150,
     });
+    clearCompleted();
 });
 
 // Funciones
@@ -37,78 +38,85 @@ function validateInput(e){
             id : Date.now()
         };
         if(dataInput.msg !== ''){
-            listArray = [...listArray, dataInput];
-            addList(listArray);
+            creatingHTML(dataInput);
+            newTodoInput.value = '';
         };
     };
 };
 
-function addList(arrList){
-    clearHTML();
-    arrList.forEach(message => {
-        const spanTodoDiv = document.createElement('span');
-        spanTodoDiv.classList.add('to-do');
-        spanTodoDiv.dataset.id = message.id;
-        spanTodoDiv.innerHTML += `
-            <div class="radio-list">
-                <span class="radio-list-img">
-                     <img class="hidden" src="/images/icon-check.svg" alt="checked">
-                </span>
-            </div>
-            <p>${message.msg}</p>
-            <img class="deleteBtn" data-id=${message.id} src="/images/icon-cross.svg" alt="delete">
-        `;
+function creatingHTML(dataInput){
+    const spanTodoDiv = document.createElement('span');
+    spanTodoDiv.classList.add('to-do');
+    spanTodoDiv.dataset.id = dataInput.id;
+    spanTodoDiv.innerHTML += `
+        <div class="radio-list" data-id="${dataInput.id}">
+            <span class="radio-list-img" data-id="${dataInput.id}">
+            </span>
+        </div>
+        <p>${dataInput.msg}</p>
+        <img class="deleteBtn" data-id="${dataInput.id}" src="/images/icon-cross.svg" alt="delete">
+    `;
 
-        listSortable.appendChild(spanTodoDiv);
-    });
+    listArray = [...listArray, spanTodoDiv];
+    
+    // Print to DOM
+    printHTML();
 
-    // Counter
-    updateCounter();
-
-    // Radio checked and delete list
+    // Checked, delete and more..
     if(listArray.length){
-        checkRadio();
+        checkRadio(spanTodoDiv);
         deleteList();
     }
-}
+};
 
-function checkRadio() {
-    const radioChecked = document.querySelectorAll('.radio-list');
-    radioChecked.forEach(elementList => {
-        elementList.addEventListener('click', e => {
-            let focusElement = e.target;
-            if(focusElement.classList.contains('radio-list')){
-                if(focusElement.classList.contains('gradient-background')){
-                    focusElement.classList.remove('gradient-background');
-                    focusElement.nextElementSibling.classList.remove('strike-paraph');
-                    focusElement.children[0].classList.add('visible');
-                    focusElement.children[0].classList.remove('hidden');
-                } else {
-                    focusElement.classList.add('gradient-background');
-                    focusElement.nextElementSibling.classList.add('strike-paraph');
-                    focusElement.children[0].classList.remove('visible');
-                    focusElement.children[1].children[0].classList.add('hidden');
-                }
-            } else {
-                console.log();
-                focusElement.parentNode.classList.add('gradient-background');
-                focusElement.parentNode.nextElementSibling.classList.add('strike-paraph');
-                focusElement.classList.remove('visible');
-                focusElement.classList.add('hidden');
-            }
-        })
-    });
+function printHTML(){
+    listArray.forEach(list => listSortable.appendChild(list));
+    updateCounter();
+};
+
+function checkRadio(spanDiv) {
+    let spanChildren = spanDiv.children[0];
+    spanChildren.onclick = e => addingStyles(e);
+};
+
+function addingStyles(list){
+    list = list.target;
+    if(list.classList.contains('radio-list')){
+        if(list.classList.contains('gradient-background')){
+            list.classList.remove('gradient-background');
+            list.nextElementSibling.classList.remove('strike-paraph');
+            list.children[0].classList.add('visible');
+            list.children[0].classList.remove('hidden');
+        } else {
+            list.classList.add('gradient-background');
+            list.nextElementSibling.classList.add('strike-paraph');
+            list.children[0].classList.remove('visible');
+            list.children[0].classList.add('hidden');
+        }
+    } else {
+        if(list.parentNode.classList.contains('gradient-background')){
+            list.parentNode.classList.remove('gradient-background');
+            list.parentNode.nextElementSibling.classList.remove('strike-paraph');
+            list.classList.add('visible');
+            list.classList.remove('hidden');
+        } else {
+            list.parentNode.classList.add('gradient-background');
+            list.parentNode.nextElementSibling.classList.add('strike-paraph');
+            list.classList.remove('visible');
+            list.classList.add('hidden');
+        }
+    }
+    
 }
 
 function deleteList(){
     const btnDelete = document.querySelectorAll('.deleteBtn');
     btnDelete.forEach(button => {
         button.addEventListener('click', e => {
-            elementId = Number(e.target.getAttribute('data-id'));
-            listArray = listArray.filter(list => list.id !== elementId);
-            if (Number(e.target.parentNode.getAttribute('data-id')) === elementId) {
-                e.target.parentNode.remove()
-            }
+            elementId = e.target.getAttribute('data-id');
+            listArray = listArray.filter(list => list.getAttribute('data-id') !== elementId);
+            clearHTML();
+            printHTML();
             updateCounter();
         });
     })
@@ -116,6 +124,25 @@ function deleteList(){
 
 function updateCounter(){
     counterList.textContent = listArray.length;
+}
+
+function clearCompleted(){
+    const clearCompletedBtn = document.querySelector('.clear-completed');
+    clearCompletedBtn.addEventListener('click', () => {
+        arrayCompletedList = document.querySelectorAll('.gradient-background');
+        if(arrayCompletedList.length){
+            let arrayDivCompleted = [];
+            arrayCompletedList.forEach(completed => {
+                arrayDivCompleted = [...arrayDivCompleted, completed.parentNode.getAttribute('data-id')];
+            });
+            arrayDivCompleted.forEach(completedId => {
+                listArray = listArray.filter(list => list.getAttribute('data-id') !== completedId);
+            });
+            clearHTML();
+            printHTML();
+            updateCounter();
+        }
+    })
 }
 
 function clearHTML() {
